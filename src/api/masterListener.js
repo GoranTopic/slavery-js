9837f88d33cf441f8cfdfde3a4ec45bd28b12795
Goran Topic <telegonicaxx@live.com>
2024-05-20 11:15:26 -0700

    made into switch

--- src/api/masterListener.js
+++ src/api/masterListener.js
@@ -1,8 +1,13 @@
 const apiListener = (process, master) => {
     process.on('message', ({ msg }) => {
-        //console.log('running on from master', msg)
-        if (msg === 'areYouMaster?')
-            process.send('I am master')
+        // make case statement for each
+        switch (msg) {
+            case 'areYouMaster?':
+                process.send('I am master')
+                break;
+            default:
+                throw new Error('Invalid message from primary process');
+        }
     });
 }
 
