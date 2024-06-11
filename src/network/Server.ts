f395f445f7051d9de1bce6d5f954a5eac34e6d8a
Goran Topic <telegonicaxx@live.com>
2024-06-11 08:52:05 -0700

    working

--- src/network/Server.ts
+++ src/network/Server.ts
@@ -1,35 +1,54 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
-import { 
+import { Socket } from "socket.io";
+import Connection from "./Connection";
 
 
-class Server {
+
+class ServerSocketIO {
+    /* 
+     * this class will handle the logic managing the server conenctions with clilent, 
+     * it will keep track of the node id and it will handle connection and dicoections
+     */
   private io: Server;
   private host: string;
   private port: number;
   private maxTransferSize: number;
+  private connectionPool 
   public name: string;
   public id: string;
-  public isOverLand: boolean;
+  
+  public isOverLan: boolean;
   public connectionCallback: any;
+  private ioOptions: any;
 
-  constructor({ host, port, maxTransferSize, name, id, isOverLand }) {
-    this.io = new Server(createServer());
-    this.host = host || "localhost";
-    this.port = port || 3000;
-    this.maxTransferSize = maxTransferSize || 1e9;
-    this.name = name;
-    this.isOverLand = isOverLand;
-    this.id = id;
-    this.connectionCallback = null;
-    // initiate the server
-    if(
+  constructor({ host, port, maxTransferSize, name, id, isOverLand }: any) {
+      this.host = host || "localhost";
+      this.port = port || 3000;
+      this.maxTransferSize = maxTransferSize || 1e9;
+      this.name = name;
+      this.isOverLan = this.host !== 'localhost'
+      this.id = id;
+      this.connectionCallback = null;
+      this.ioOptions = {
+          maxHttpBufferSize: this.maxTransferSize,
+      };
+      // initiate the server
+      if(this.isOverLan){
+          this.io = new Server(createServer(), this.ioOptions);
+      }else{
+          this.io = new Server(this.port, this.ioOptions);
+      }
+      // create a new socket.io client instance
+      this.io.on("connection", this._handleConnection.bind(this));
+      this.io.on("reconnect", () => console.log("[master] on reconnect triggered"));
   }
 
-  init() {
-      // create a new socket.io client instance
-      this.io.on("connection", this._handleSocketConnection.bind(this));
-      this.io.on("reconnect", () => log("[master] on reconnect triggered"));
+  _handleConnection(socket: Socket) {
+      // make a new connectection instance
+      let connection = new Connection(socket, this);
+      // run callback
+      this.connectionCallback(connection);
   }
 
 
@@ -38,15 +57,29 @@ class Server {
       this.io.emit('_exit');
       // close all sockets
       this.io.close();
-      // close all processes
-      process.send('exit');
       // exit process
       process.exit();
   }
 
-  async _handleSocketConnection(socket) {
-
-
+  async connected(number=1) {
+      return new Promise((resolve, reject) => {
+          let interval : any;
+          let timeout : number;
+          // set interval to check for connection
+          interval = setInterval(() => {
+              if(this.pool.size() >= number) {
+                  clearInterval(interval);
+                  clearTimeout(timeout);
+                  resolve();
+              }
+          }, this.heartBeat);
+          // set timeout to reject if no connection
+          timeout = setTimeout(() => {
+              clearInterval(interval);
+              reject('timeout');
+          }
+            , 1000 * 60 ); // 1 minute
+      });
   }
 
 }
